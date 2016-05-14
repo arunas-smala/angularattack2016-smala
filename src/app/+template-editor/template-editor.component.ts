35df58c626e2fa68f2fef979ef8a4057e439ebe2
Darius Smaliukas <darius@mailerlite.com>
2016-05-14 13:01:46 +0300

    template editor save load data

--- src/app/+template-editor/template-editor.component.ts
+++ src/app/+template-editor/template-editor.component.ts
@@ -1,27 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
+
 
 @Component({
     moduleId: module.id,
     selector: 'app-template-editor',
     templateUrl: 'template-editor.component.html',
+    providers: [HTTP_PROVIDERS],
     styleUrls: ['template-editor.component.css']
 })
 export class TemplateEditorComponent implements OnInit {
 
-    public template: string;
+    template:string;
 
-    constructor() {}
+    constructor(private http:Http) {
+    }
 
     ngOnInit() {
-        let template = localStorage.getItem('template');
-
-        if (template) {
-            this.template = template;
-        }
+        this._loadTemplate();
     }
 
     onSubmit() {
+        if (!this.template) {
+            alert('Empty!'); // TODO change to normal alert
+
+            // let's load not empty template
+            this._loadTemplate();
+
+            return;
+        }
+
         localStorage.setItem('template', this.template);
+
+        alert('Saved!'); // TODO change to normal alert
     }
 
+    private _loadTemplate() {
+        this._getStorageTemplate()
+            .then((template) => {
+                if (typeof template === 'string') {
+                    this.template = template;
+                }
+            }).catch(() => {
+            this._getFileTemplate()
+                .subscribe(response => {
+                    this.template = response.text()
+                });
+        })
+    }
+
+    private _getStorageTemplate() {
+        return new Promise(
+            function (resolve, reject) {
+                let template = localStorage.getItem('template');
+                if (typeof template === 'string') {
+                    resolve(template);
+                } else {
+                    reject();
+                }
+            }
+        );
+    }
+
+    private _getFileTemplate() {
+        return this.http.get('/default_template.html');
+    }
 }
