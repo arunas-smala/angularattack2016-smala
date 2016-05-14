c9fef79688ab2c15b28945ed6b63dd7cf8c4d317
Arunas Smaliukas <arunas.smaliukas@gmail.com>
2016-05-14 04:58:21 +0300

    just style

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { TemplateBuilderAppComponent, environment } from './app/';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrap(TemplateBuilderAppComponent);
