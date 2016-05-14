e713c5f9b14188739dc35b38f1b20938789eb0d2
Darius Smaliukas <darius@mailerlite.com>
2016-05-14 11:35:28 +0300

    start of alertify

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -13,7 +13,10 @@ module.exports = function(defaults) {
       'rxjs/**/*.js',
       '@angular/**/*.js',
       'moment/moment.js',
-      'ng2-bootstrap/**/*.js'
+      'ng2-bootstrap/**/*.js',
+      'alertify/lib/alertify.min.js',
+      'alertify/themes/alertify.core.css',
+      'alertify/themes/alertify.bootstrap.css'
     ]
   });
 };
