342fb24e6bcfb60edb6eae9e2cb0fc0bc26de760
Arunas Smaliukas <arunas.smaliukas@gmail.com>
2016-05-14 18:03:34 +0300

    block directive

--- src/app/+drag-n-drop/template/template-language/block/block.directive.ts
+++ src/app/+drag-n-drop/template/template-language/block/block.directive.ts
@@ -1,20 +1,57 @@
-import { Directive, OnInit, Input } from '@angular/core';
+import { 
+    Component, 
+    Directive, 
+    OnInit, 
+    Input,
+    Output
+} from '@angular/core';
+import { DataService } from '../../../shared/index';
+import { Scope } from '../../../shared/index';
 
 @Directive({
-    selector: 'block,[block]'
+    selector: 'block,[block]',
+    exportAs: 'block'
 })
 export class BlockDirective implements OnInit {
 
     @Input()
-    public name: string;
+    public name:string;
 
     @Input()
-    public title: string;
+    public title:string;
 
-    constructor() {}
+    public variables:any;
+    public blocks:any;
+    public repeaters:any;
+
+    private parentScope:any;
+
+    constructor(
+        private service: DataService
+    ) {}
 
     ngOnInit() {
-        console.log('we have it', this.name);
+        // Temporary store current active scope, so we would be able to restore
+        // it after this block will be compiled
+        this.parentScope = this.service.getTraverseCursor();
+        // Yes, yes, blocks and only blocks create new scopes
+        let newScope = new Scope();
+
+        // Assign scope data to be exported
+        this.variables = newScope.variables;
+        this.blocks = newScope.blocks;
+        this.repeaters = newScope.repeaters;
+
+        // Add this block to parent scope
+        this.parentScope.blocks[ this.name ] = newScope;
+        // Settings current scope to this block. So new inner blocks will
+        // belong to this block
+        this.service.setTraverseCursor(newScope);
+    }
+
+    ngAfterViewInit() {
+        // Restore old scope
+        this.service.setTraverseCursor(this.parentScope);
     }
 
 }
--- src/app/+drag-n-drop/template/template.component.ts
+++ src/app/+drag-n-drop/template/template.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { 
+    Component, 
+    OnInit, 
+    ViewEncapsulation,
+    AfterViewInit
+} from '@angular/core';
 import { TEMPLATE_DIRECTIVES } from './template-language/index';
+import { DataService } from '../shared/data.service';
 
 @Component({
     moduleId: module.id,
@@ -9,11 +15,18 @@ import { TEMPLATE_DIRECTIVES } from './template-language/index';
     directives: [TEMPLATE_DIRECTIVES],
     encapsulation: ViewEncapsulation.Native
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent implements OnInit, AfterViewInit {
 
-  constructor() {}
+  constructor(private _service: DataService) {}
 
   ngOnInit() {
+      this._service.startTraverse();
+      console.log('template start');
+  }
+
+  ngAfterViewInit() {
+      this._service.endTraverse();
+      console.log('template finish2');
   }
 
 }
