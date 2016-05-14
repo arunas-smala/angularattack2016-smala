8a3b96a779a31568ce5e5d7fe651c19051c69b67
Arunas Smaliukas <arunas.smaliukas@gmail.com>
2016-05-14 04:56:19 +0300

    base layout

--- src/app/template-builder.component.ts
+++ src/app/template-builder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
@@ -6,7 +6,8 @@ import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
   selector: 'template-builder-app',
   templateUrl: 'template-builder.component.html',
   styleUrls: ['template-builder.component.css'],
-  directives: [AlertComponent]
+  directives: [AlertComponent],
+  encapsulation: ViewEncapsulation.None
 })
 export class TemplateBuilderAppComponent {
   title = 'template-builder works!';
