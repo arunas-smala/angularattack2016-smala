b85a307254b7c60b7acfe6101b57fa976817441c
Arunas Smaliukas <arunas.smaliukas@gmail.com>
2016-05-16 01:21:36 +0300

    variables outside blocks

--- src/app/+drag-n-drop/template/template.component.ts
+++ src/app/+drag-n-drop/template/template.component.ts
@@ -12,7 +12,7 @@ import { getTemplate } from '../../shared/templates';
 @Component({
     moduleId: module.id,
     selector: 'tb-template',
-    template: getTemplate(),
+    template: getTemplate().replace(/\[\[/gm, '{{').replace(/\]\]/gm, '}}'),
     styleUrls: ['template.component.css'],
     directives: [TEMPLATE_DIRECTIVES, BlockComponent],
     encapsulation: ViewEncapsulation.Native
@@ -21,8 +21,11 @@ export class TemplateComponent implements OnInit, AfterViewInit {
 
   constructor(private service: DataService) {}
 
+  public variables:any;
+
   ngOnInit() {
       this.service.startTraverse();
+      this.variables = this.service.getData().variables;
   }
 
   ngAfterViewInit() {
