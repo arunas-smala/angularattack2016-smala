de3d009e6a3ab10ab8638e90f7da9cdc1198b45e
Darius Smaliukas <darius@mailerlite.com>
2016-05-15 12:31:33 +0300

    updates for styel

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -16,7 +16,6 @@ module.exports = function(defaults) {
       'ng2-bootstrap/**/*.js',
       'codemirror/lib/codemirror.js',
       'codemirror/lib/codemirror.css',
-      'codemirror/theme/ambiance.css',
       'codemirror/mode/htmlmixed/htmlmixed.js',
       'codemirror/mode/xml/xml.js',
       'codemirror/mode/javascript/javascript.js',
