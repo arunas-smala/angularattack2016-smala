f4f9d18ae0af1a55a98c9d6e7a7e4dc78a039c7d
Darius Smaliukas <darius@mailerlite.com>
2016-05-15 11:26:05 +0300

    localTemplate, fileTemplate updates

--- src/app/+template-editor/template-editor.component.ts
+++ src/app/+template-editor/template-editor.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Http, HTTP_PROVIDERS} from '@angular/http';
 import { CodemirrorComponent } from './shared/index'
+import { localTemplate, fileTemplate } from './../shared/templates';
 
 @Component({
     moduleId: module.id,
@@ -18,16 +19,12 @@ export class TemplateEditorComponent implements OnInit {
     }
 
     ngOnInit() {
-        this._loadTemplate();
+        this.loadTemplate();
     }
 
     onSubmit() {
         if (!this.template) {
             console.log('Empty!'); // TODO change to normal alert
-
-            // let's load not empty template
-            this._loadTemplate();
-
             return;
         }
 
@@ -36,34 +33,7 @@ export class TemplateEditorComponent implements OnInit {
         console.log('Saved!'); // TODO change to normal alert
     }
 
-    private _loadTemplate() {
-        this._getStorageTemplate()
-            .then((template) => {
-                if (typeof template === 'string') {
-                    this.template = template;
-                }
-            }).catch(() => {
-            this._getFileTemplate()
-                .subscribe(response => {
-                    this.template = response.text()
-                });
-        })
-    }
-
-    private _getStorageTemplate() {
-        return new Promise(
-            function (resolve, reject) {
-                let template = localStorage.getItem('template');
-                if (typeof template === 'string') {
-                    resolve(template);
-                } else {
-                    reject();
-                }
-            }
-        );
-    }
-
-    private _getFileTemplate() {
-        return this.http.get('/default_template.html');
+    private loadTemplate() {
+        this.template = localTemplate || fileTemplate;
     }
 }
